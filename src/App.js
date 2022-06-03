e493704a89b40b5de31f4b522d077226c0284516
Alejandro Ramirez <zdro1216@gmail.com>
2022-06-03 00:03:32 -0700

    setting up nav bar

--- src/App.js
+++ src/App.js
@@ -5,10 +5,11 @@ import { TooltipComponent } from '@syncfusion/ej2-react-popups'
 import './App.css'
 
 const App = () => {
+  const activeMenu = false;
   return (
     <div>
       <BrowserRouter>
-      
+      {/* //Gives shape and styling to settings button */}
       <div className='flex relative dark:bg-main-dark-bg'>
         <div className='fixed right-4 bottom-4' style={{zIndex:'1000'}}>
           <TooltipComponent content='Settings' position = 'Top'>
@@ -18,12 +19,62 @@ const App = () => {
 
 
         </TooltipComponent>
+        </div >
+        {/* decides whether the menu should be opened or closed */}
+        {activeMenu ? (
+          <div className='w-72 fixed sidebar dark:bg-secondary-dark-bg bg-white'>
+          Sidebar
+          </div>
 
+        ):(
+          <div className='w-0 dark: bg-secondary-dark-bg'>
+          Sidebar w-0
+          </div>
+        )}
+
+        {/* Defining the divs for navigation bar  */}
+        <div className={
+          `dark:bg-main-bg bg-main-bg min-h-screen w-full ${activeMenu ? 'md:ml-72' : 'flex-2'}`
+        }>
+
+          <div className='fixed md:static bg-main-bg dark:bg-main-dark-bg navbar w-full'> 
+              Navbar
+          </div>
+        </div>
+        {/* Div for routing  */}
+        <div>
+
+          <Routes>
+            {/* Dashboard*/}
+            <Route path='/' element = 'Ecommerce' />
+            <Route path='/ecommerce' element = 'Ecommerce' />
+            
+            {/* Pages */}
+            <Route path='/orders' element = 'Orders' />
+            <Route path='/employees' element = 'Employees' />
+            <Route path='/customers' element = 'Customers' />
+
+            {/* Apps */}
+            <Route path = '/kanban' element = 'Kanban' />
+            <Route path = '/editor' element = 'Editor' />
+            <Route path = '/calendar' element = 'Calendar' />
+            <Route path = '/color-picker' element = 'ColorPicker' />
+
+
+            {/* Charts */}
+
+            <Route path = '/line' element = 'Line' />
+            <Route path = '/area' element = 'Area' />
+            <Route path = '/bar' element = 'Bar' />
+            <Route path = '/pie' element = 'Pie' />
+            <Route path = '/financial' element = 'Financial' />
+            <Route path = '/color-mapping' element = 'ColorMapping' />
+            <Route path = '/pyramid' element = 'Pyramind' />
+            <Route path = '/pyramid' element = 'Pyramind' />
+
+          </Routes>
 
         </div>
-
-        {activeMenu ? }
-
 
       </div>
       
