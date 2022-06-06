1c3b41fb78674a1132a6fc329819d2a461cb3deb
zdro1216 <zdro1216@gmail.com>
2022-06-06 07:47:15 -0700

    updated navbar

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -8,6 +8,8 @@ import { links} from '../data/dummy'
 
 const Sidebar = () => {
   const activeMenu = true;
+  const activeLink = 'flex items-center gap-5 pl-4 pt-4 pb-2.5 rounded-lg text-white text-md m-2'
+  const normalLink = 'flex items-center gap-5 pl-4 pt-4 pb-2.5 rounded-lg  text-md text-gray-700 dark:text-gray-200 dark:hover:text-black hover:bg-ligh-gray m-2'
   return (
     <div className='ml-3 h-screen md:overflow-hidden overflow-auto md:hover:overflow-auto pb-10'>
 
@@ -38,9 +40,13 @@ const Sidebar = () => {
               {item.links.map((link)=>(
                 <NavLink 
                   to={`/${link.name}`} 
-                  key= {link.name} 
-                  onClick={()=>{}}>
-
+                  key={link.name} 
+                  onClick={() => {}}
+                  className = {({isActive})=>  isActive ? activeLink : normalLink}
+                  >
+                    <span className='capitalize'>
+                      {link.name}
+                    </span>
                 </NavLink>
 
               ))}
